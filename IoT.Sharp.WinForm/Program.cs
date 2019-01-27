0698cb5bb0e6148832d8a72f789822360fbcbbe8
MysticBoy <mysticboy@live.com>
2019-01-27 19:17:42 +0800

    修改登录地址

--- IoT.Sharp.WinForm/Program.cs
+++ IoT.Sharp.WinForm/Program.cs
@@ -8,19 +8,23 @@ using IoT.Sharp.Sdk.CSharp;
 
 namespace IoT.Sharp.WinForm
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             BonusSkins.Register();
+#if DEBUG
             SdkClient.BaseURL = Properties.Settings.Default.ApiUrl;
+#else
+            SdkClient.BaseURL = "http://39.98.211.23:5050";
+#endif
             Application.Run(new frmMain());
         }
     }
